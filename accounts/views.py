710c393f848dabadbb9fa004313f27282722ed9d
pillumina <derios1230@gmail.com>
2018-03-03 11:44:49 -0800

    Django deep space fans community

---
+++ accounts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import CreateView
+
+
+from . import forms # create the forms.py and connect login and logout
+# to actual view.
+
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
